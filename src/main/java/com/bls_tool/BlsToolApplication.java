f34bf9e72310b217585f6fdebda1a4b0c9b85a80
8fuhst <59779937+8fuhst@users.noreply.github.com>
2021-05-10 15:59:37 +0200

    Rudimentary Server Backend

---
+++ src/main/java/com/bls_tool/BlsToolApplication.java
@@ -0,0 +1,13 @@
+package com.bls_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlsToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlsToolApplication.class, args);
+    }
+
+}
