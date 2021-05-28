3e054b017b4f9fe1fb9cc5211df94c370afbb412
8fuhst <59779937+8fuhst@users.noreply.github.com>
2021-05-28 15:57:28 +0200

    Added functionality for newest verdicts

--- src/main/java/com/bls_tool/controller/SearchController.java
+++ src/main/java/com/bls_tool/controller/SearchController.java
@@ -5,8 +5,11 @@ import com.bls_tool.model.type.Verdict;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 // @RequestMapping("/search")
@@ -26,6 +29,18 @@ public class SearchController {
         // Verdict verdict1 = new Verdict();
         // JSONObject verdict_json = verdict1.toJsonMetadata();
         // result.add(verdict_json);
+
+        return verdictByQuery;
+    }
+
+    @GetMapping("/newest")
+    @ResponseBody
+    public Page<Verdict> newest() {
+        // JSONArray result = new JSONArray();
+
+        Page<Verdict> verdictByQuery =
+                verdictRepository.findVerdictBy("",
+                        PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "date")));
 
         return verdictByQuery;
     }
--- src/main/java/com/bls_tool/controller/VerdictController.java
+++ src/main/java/com/bls_tool/controller/VerdictController.java
@@ -1,18 +1,19 @@
 package com.bls_tool.controller;
 
 import com.bls_tool.model.type.Verdict;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import net.minidev.json.JSONObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/verdict")
 public class VerdictController {
 
     @GetMapping
-    public String verdict() {
+    public JSONObject verdict() {
         Verdict verdict1 = new Verdict();
-        return verdict1.toJSON().toJSONString();
+        return verdict1.toJSON();
     }
 
 }
