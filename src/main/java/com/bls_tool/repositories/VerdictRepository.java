b3cd1be9aed9542a9056089d9a66ae8de0d52440
Hendrik Brandt <45638790+hendrikbrandt@users.noreply.github.com>
2021-05-31 15:52:54 +0200

    Add files via upload

--- src/main/java/com/bls_tool/repositories/VerdictRepository.java
+++ src/main/java/com/bls_tool/repositories/VerdictRepository.java
@@ -3,10 +3,20 @@ package com.bls_tool.repositories;
 import com.bls_tool.model.type.Verdict;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface VerdictRepository extends ElasticsearchRepository<Verdict, String> {
     Page<Verdict> findVerdictBy(String query, Pageable pageable);
+
+    // Page<Verdict> findVerdictBydocumentnumber(String query, Pageable pageable);
+
+    Page<Verdict> findVerdictByDate(String query, Pageable pageable);
+
+    //Verdict findVerdictByDocumentnumber(String query);
+
+    @Query("{\"query\": {\"match\": [{\"documentnumber\": {\"query\": docnr}}]}}")
+    Verdict findVerdictByDocumentnumber(String docnr);
 
     // @Query
     // Page<Verdict> findVerdictByCustomQuery(String query);
