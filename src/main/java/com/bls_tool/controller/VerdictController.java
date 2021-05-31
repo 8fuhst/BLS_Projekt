231ffd3213cc9e5dbe4376fb6d3b29b621871a6d
Hendrik Brandt <45638790+hendrikbrandt@users.noreply.github.com>
2021-05-31 15:52:19 +0200

    Add files via upload

--- src/main/java/com/bls_tool/controller/VerdictController.java
+++ src/main/java/com/bls_tool/controller/VerdictController.java
@@ -1,7 +1,9 @@
 package com.bls_tool.controller;
 
 import com.bls_tool.model.type.Verdict;
+import com.bls_tool.repositories.VerdictRepository;
 import net.minidev.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
@@ -9,11 +11,23 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/verdict")
 public class VerdictController {
-
-    @GetMapping
+    @Autowired
+    VerdictRepository verdictRepository;
+    /*@GetMapping("/verdict")
     public JSONObject verdict() {
         Verdict verdict1 = new Verdict();
         return verdict1.toJSON();
     }
 
+    @GetMapping("/verdict/" + docnr)
+    public JSONObject getSpecificVerdict() {
+
+    }
+*/
+    @GetMapping
+    @ResponseBody
+    public Verdict getSpecificVerdict(@RequestParam String documentnumber) {
+        Verdict verdict = verdictRepository.findVerdictByDocumentnumber(documentnumber);
+        return verdict;
+    }
 }
