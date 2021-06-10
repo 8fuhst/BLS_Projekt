ee048513b8d72ee7b24114fc2507a4adcb5f25f4
8fuhst <59779937+8fuhst@users.noreply.github.com>
2021-06-10 16:35:24 +0200

    Versuche das Ding zu Deployen mit Anzeige der Daten aus ES

---
+++ bls_backend/src/main/java/com/bls_tool/model/type/VerdictNode.java
@@ -0,0 +1,42 @@
+package com.bls_tool.model.type;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Document(indexName = "verdict_nodes2")
+public class VerdictNode {
+    @Id
+    private String _id;
+    @Field(type = FieldType.Text, name = "filenumber")
+    private String filenumber;
+    @Field(type = FieldType.Text, name = "outgoing_reference_list")
+    private String[] outgoingReferenceList;
+    @Field(type = FieldType.Text, name = "outgoing_reference_set")
+    private String[] outgoingReferenceSet;
+    @Field(type = FieldType.Text, name = "incoming_reference_set")
+    private String[] incomingReferenceSet;
+    @Field(type = FieldType.Integer, name = "incoming_count")
+    private int incomingCount;
+
+    public String getFilenumber() {
+        return filenumber;
+    }
+
+    public String[] getOutgoingReferenceList() {
+        return outgoingReferenceList;
+    }
+
+    public String[] getOutgoingReferenceSet() {
+        return outgoingReferenceSet;
+    }
+
+    public String[] getIncomingReferenceSet() {
+        return incomingReferenceSet;
+    }
+
+    public int getIncomingCount() {
+        return incomingCount;
+    }
+}
